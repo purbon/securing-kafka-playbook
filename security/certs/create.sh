94da1d5226ea080cdc46e7b45bb51f620a87f401
Pere Urbon <pere@confluent.io>
2020-10-29 10:47:24 +0100

    add scripts for simple store generation

---
+++ security/certs/create.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Cleanup files
+rm -f *.crt *.csr *_creds *.jks *.srl *.key *.pem *.der *.p12 *.log
+
+# Generate CA key
+openssl req -new -x509 -keyout snakeoil-ca-1.key -out snakeoil-ca-1.crt -days 365 -subj '/CN=ca1.test.confluentdemo.io/OU=TEST/O=CONFLUENT/L=PaloAlto/ST=Ca/C=US' -passin pass:confluent -passout pass:confluent
+
+users=(kafka client schemaregistry connect connector controlcenter thusnelda)
+echo "Creating certificates"
+printf '%s\0' "${users[@]}" | xargs -0 -I{} -n1 -P15 sh -c './create-cert.sh "$1" > "certs-create-$1.log" 2>&1 && echo "Created certificates for $1"' -- {}
+echo "Creating certificates completed"
