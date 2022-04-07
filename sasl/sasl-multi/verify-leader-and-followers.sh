bc3aaab87f1e0041d61d4f6d02cb13a3ef04366c
Pere Urbon <pere@confluent.io>
2022-04-07 18:39:12 +0200

    add two sasl models

---
+++ sasl/sasl-multi/verify-leader-and-followers.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo "zookeeper0 status: "
+docker-compose exec zookeeper00 echo mntr | nc localhost 2181 | grep zk_server_state
+
+echo "zookeeper1 status: "
+docker-compose exec zookeeper01 echo mntr | nc localhost 2182 | grep zk_server_state
+
+echo "zookeeper2 status: "
+docker-compose exec zookeeper02 echo mntr | nc localhost 2183 | grep zk_server_state
+
+
+echo "zookeeper0 sync followers: "
+docker-compose exec zookeeper   echo mntr | nc localhost 2181 | grep zk_synced_followers
+
+echo "zookeeper1 sync followers: "
+docker-compose exec zookeeper01 echo mntr | nc localhost 2182 | grep zk_synced_followers
+
+echo "zookeeper2 sync followers: "
+docker-compose exec zookeeper02 echo mntr | nc localhost 2183 | grep zk_synced_followers
