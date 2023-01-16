eaddc0de68060c1d05e4bee6eca65171ad67c63e
Pere Urbon <pere@confluent.io>
2023-01-16 14:14:00 +0100

    add a second node into the big deployment one

--- kraft/scripts/create-topics.sh
+++ kraft/scripts/create-topics.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 
-docker exec kafka4 kafka-topics --bootstrap-server kafka4:29095  --create --topic bar --partitions 3
+docker exec kafka4 kafka-topics --bootstrap-server kafka4:29095  --create --topic bar --partitions 3 --replication-factor 2
 docker exec kafka4 kafka-topics --bootstrap-server kafka4:29095 --describe
