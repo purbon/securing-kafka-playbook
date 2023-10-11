8eed000fad221792ceab11c1d730ac169390dcf3
Pere Urbón-Bayes <pere.urbon@gmail.com>
2023-10-11 15:39:32 +0200

    add template for tls dynamic cert rotation example

--- security/certs/create.sh
+++ security/certs/create.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
 # Cleanup files
-rm -f *.crt *.csr *_creds *.jks *.srl *.key *.pem *.der *.p12 *.log
+
+rm -rf things
+mkdir -p things
+#cd things && rm -f *.crt *.csr *_creds *.jks *.srl *.key *.pem *.der *.p12 *.log
+#cd ..
 
 # Generate CA key
 openssl req -new -x509 -keyout snakeoil-ca-1.key -out snakeoil-ca-1.crt -days 1825 -subj '/CN=ca1.test.confluentdemo.io/OU=TEST/O=CONFLUENT/L=PaloAlto/ST=Ca/C=US' -passin pass:confluent -passout pass:confluent
 
 users=(ldap kafka client schemaregistry connect connector controlcenter mds thusnelda zookeeper)
+cd things
+
 echo "Creating certificates"
-printf '%s\0' "${users[@]}" | xargs -0 -I{} -n1 -P15 sh -c './create-cert.sh "$1" > "certs-create-$1.log" 2>&1 && echo "Created certificates for $1"' -- {}
+printf '%s\0' "${users[@]}" | xargs -0 -I{} -n1 -P15 sh -c '../create-cert.sh "$1" > "certs-create-$1.log" 2>&1 && echo "Created certificates for $1"' -- {}
 echo "Creating certificates completed"
+cd ..
---
+++ tls-dynamic-cert-rotation/update-certs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+
+cp ../security/certs/things/kafka.keystore.jks certs/kafka.keystore 
+cp ../security/certs/things/kafka.truststore.jks certs/kafka.truststore                                         
+
+cp ../security/certs/things/client.keystore.jks certs/client.keystore 
+cp ../security/certs/things/client.truststore.jks certs/client.truststore  
